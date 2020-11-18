1b99be99784c6b02968a3e02beede73bc25e8a57
SedivyA <sedivya.98@spst.eu>
2020-11-18 10:33:28 +0100

    Přejmenovla jsem labely

--- Soucin_1/Form1.Designer.cs
+++ Soucin_1/Form1.Designer.cs
@@ -57,9 +57,9 @@ namespace Soucin_1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Soucin";
             // 
             // button1
             // 
@@ -76,27 +76,27 @@ namespace Soucin_1
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(13, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Podil";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(13, 101);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "Soucet";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(13, 118);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(36, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.label4.Text = "Rozdil";
             // 
             // Form1
             // 
